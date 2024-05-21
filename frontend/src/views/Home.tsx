996f747fde17e06d19f39cd5e96be8f4d5601b50
Mykola Chub <mykolachubb@gmail.com>
2024-05-22 00:55:08 +0300

    feat: add home file upload scratch logic

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,7 +1,39 @@
+import axios from 'axios';
 import React from 'react';
+import authStore from '../stores/auth';
 
 const Home = () => {
-  return <div>Home</div>;
+  const { token } = authStore();
+  const handleFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const file = e.target.files[0];
+      console.log(file);
+
+      const formData = new FormData();
+      formData.append('file', file);
+      formData.append('lastModified', file.lastModified.toString());
+
+      try {
+        const sent = await axios.post(
+          'http://localhost:8080/api/meta',
+          formData,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
+        console.log(sent);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+  return (
+    <div>
+      <input type="file" name="file" id="file" onChange={handleFile} />
+    </div>
+  );
 };
 
 export default Home;
